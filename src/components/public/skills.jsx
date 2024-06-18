368efee54dec0cae376ae786ccc9844dd2f66c85
Hanh NG <moitmp64@gmail.com>
2024-06-18 15:51:59 +0200

    fix reponsive mobile

--- src/components/public/skills.jsx
+++ src/components/public/skills.jsx
@@ -18,10 +18,10 @@ const Skills = () => {
         Mes compétences
       </h2>
       <div className="flex flex-col justify-center items-center md:flex-row md:justify-center md:items-center md:mb-10 mx-auto">
-        <img className="h-[300px] object-contain" src={frontend} alt="logo de Frontend" />
+        <img className="h-[300px] w-[300px] object-contain" src={frontend} alt="logo de Frontend" />
         <div className="hidden md:block anim-gradient-bar-horizontal mb-4 mx-10"></div>
         <div className="anim-gradient-bar-vertical mt-4 mb-4 mx-10 md:hidden"></div>
-        <img className="h-[300px] object-contain" src={backend} alt="logo de Backend" />
+        <img className="h-[300px] w-[300px] object-contain" src={backend} alt="logo de Backend" />
       </div>
       <div className="bg-tertiary pt-0.5 pb-10">
         <h2 className="relative z-0 text-1xl w-fit mx-auto mb-7 mt-10 sm:text-3xl sm:mb-10 md:text-4xl md:mb-12 after:absolute after:w-full after:h-2 after:bottom-0 after:left-0 after:-z-10 after:bg-main-light sm:after:h-5 sm:after:-bottom-2">
