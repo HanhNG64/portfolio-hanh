054ad66403a47d68b5fa9e2e6fa7a4cdc38d7d2f
Hanh NG <moitmp64@gmail.com>
2024-06-22 14:57:17 +0200

    MAJ API_URL

--- src/_utils/constants.jsx
+++ src/_utils/constants.jsx
@@ -1,4 +1,4 @@
-const API_URL = 'https://p8-portfolio-api.vercel.app';
+const API_URL = 'https://api-portfolio-tawny.vercel.app';
 export const API_ROUTES = {
   SIGN_UP: `${API_URL}/api/auth/signup`,
   SIGN_IN: `${API_URL}/api/auth/login`,
